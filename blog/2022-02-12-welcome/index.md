1f9fde657b00529c2e7cea9c3b01ca26c3e80927
XZH <1378653041@qq.com>
2022-02-25 22:27:48 +0800

    frist push

---
+++ blog/2022-02-12-welcome/index.md
@@ -0,0 +1,8 @@
+---
+slug: welcome
+title: 🎉欢迎来到我的博客🎉
+authors: [XZH]
+tags: []
+---
+
+<!-- truncate -->
---
+++ docusaurus.config.js
@@ -0,0 +1,75 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "ZXK的个人技术博客",
+  tagline: "此博客是利用docusaurusV2框架搭建",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "dsxksss", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        // docs: {
+        //   sidebarPath: require.resolve("./sidebars.js"),
+        //   // Please change this to your repo.
+        //   editUrl:
+        //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        // },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          routeBasePath: "/",
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        hideOnScroll: true,
+        title: "😎XZH的博客😎",
+        items: [
+          // {
+          //   type: "doc",
+          //   docId: "intro",
+          //   position: "left",
+          //   label: "Tutorial",
+          // },
+
+          {
+            href: "https://github.com/XIE-ZI-HAO",
+            label: "我的GitHub主页",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        copyright: `Copyright © ${new Date().getFullYear()} XZHBLOG`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
